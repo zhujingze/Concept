285bb7014c46f04f88b7b1c6c89ffc3f01be17b0
zhujingze <73783778+zhujingze@users.noreply.github.com>
2025-01-09 16:55:00 +0800

    Create train.sh

---
+++ train.sh
@@ -0,0 +1,9 @@
+CUDA_VISIBLE_DEVICES=7 python train.py \
+    --model 'Llama-2-7b-hf' \
+    --device 'cuda' \
+    --data_folder 'data' \
+    --subject 'global_facts' \
+    --bs 4 \
+    --lr 1e-3 \
+    --epoch  20\
+    --save_folder  "layer_weight"\
