440c53c7108b95133472ce5502bd1b4ad42c12f8
zhujingze <73783778+zhujingze@users.noreply.github.com>
2025-01-14 11:17:17 +0800

    Update dataset_mmlu.py

--- dataset_mmlu.py
+++ dataset_mmlu.py
@@ -3,7 +3,7 @@ import pandas as pd
 import torch
 
 class MultipleChoiceDataset(Dataset):
-    def __init__(self, csv_file, example_file, tokenizer, max_length=1024):
+    def __init__(self, subject, csv_file, example_file, tokenizer, max_length=1024):
         self.data = pd.read_csv(csv_file, header=None)
         self.example_data = pd.read_csv(example_file, header=None)
         self.tokenizer = tokenizer
@@ -19,12 +19,12 @@ class MultipleChoiceDataset(Dataset):
             question = self.example_data.iloc[idx, 0]
             choices = [self.example_data.iloc[idx, i+1] for i in range(4)]
             answer = self.example_data.iloc[idx, 5]
-            example_text += "<question>:\n"
+            #example_text += "<question>:\n"
             example_text += f"{question}\n"
-            example_text += "<options>:\n"
+            #example_text += "<options>:\n"
             for i, choice in enumerate(choices):
                 example_text += f"{self.choices[i]}. {choice}\n"
-            example_text += "<answer>:\n"
+            example_text += "Answer:"
             example_text += f"{answer}\n\n"
         return example_text
 
@@ -32,15 +32,15 @@ class MultipleChoiceDataset(Dataset):
         question = self.data.iloc[idx, 0]
         choices = [self.data.iloc[idx, i+1] for i in range(4)]
         answer = self.data.iloc[idx, 5]
-        prompt_text = "Choose the right answer from options the answer should be one of A, B, C, D.\n"
+        prompt_text = f"The following are multiple choice questions (with answers) about {subject}.\n\n"
         prompt_text += self.get_example()
 
-        input_text = "<question>:\n"
-        input_text += f"{question}\n"
-        input_text += "<options>:\n"
+        #input_text = "<question>:\n"
+        input_text = f"{question}\n"
+        #input_text += "<options>:\n"
         for i, choice in enumerate(choices):
             input_text += f"{self.choices[i]}. {choice}\n"
-        input_text += "<answer>: \n"
+        input_text += "Answer: \n"
         input_text = prompt_text + input_text
 
         encodings = self.tokenizer(
