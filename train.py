c3b7c45d7114e00e445c7841ff064ff411b516d9
zhujingze <73783778+zhujingze@users.noreply.github.com>
2025-01-17 15:07:06 +0800

    Update train.py

--- train.py
+++ train.py
@@ -14,6 +14,12 @@ def compute_loss(logits, labels):
     loss = criterion(logits, labels)
     return loss
 
+def compute_accuracy(logits, labels):
+    _, predicted = torch.max(logits, 1)
+    correct = (predicted == labels).sum().item()
+    accuracy = correct / labels.size(0)
+    return accuracy
+
 def main(args):
     device = args.device
     tokenizer = AutoTokenizer.from_pretrained(args.model)
@@ -50,7 +56,11 @@ def main(args):
 
     for epoch in range(args.epoch):
         model.train()
+        total_correct = 0
+        total_correct_norm = 0
+        total_samples = 0
         if args.method == 'letter':
+            # train
             for batch in train_loader:
                 optimizer.zero_grad()
                 input_ids = batch["input_ids"].to(device)
@@ -83,6 +93,32 @@ def main(args):
             print(f"End of Epoch {epoch+1}, Layer Weights:", model.layer_weights.data)
             # model.layer_weights.data = torch.clamp(model.layer_weights.data, min=0.0, max=1.0)
             # print(f"End of Epoch {epoch+1}, Layer Weights:", model.layer_weights.data / model.layer_weights.data.sun())
+
+            # test
+            for batch in train_loader:
+                optimizer.zero_grad()
+                input_ids = batch["input_ids"].to(device)
+                attention_mask = batch["attention_mask"].to(device)
+                label = batch["label"].to(device)
+    
+                out_idxs = []
+                for i in range(attention_mask.size(0)):
+                    out_idx = ((attention_mask[i] != 1).nonzero(as_tuple=True)[0])[0].item() - 1
+                    out_idxs.append(out_idx)
+        
+                outputs = model(input_ids=input_ids, attention_mask=attention_mask)
+            
+                out_idxs = torch.tensor(out_idxs, device = device)
+                out_idxs = out_idxs.unsqueeze(1)
+                  
+                logits = outputs.logits.gather(1, out_idxs.unsqueeze(-1).expand(-1, -1, outputs.logits.size(-1)).long())
+                logits = logits.squeeze(1)
+                logits = logits[:, [319, 350, 315, 360]]
+                logits = logits.to(device)
+                batch_accuracy = compute_accuracy(logits, label)
+                total_correct += (batch_accuracy * label.size(0))
+                total_samples += label.size(0)
+        
         if args.method == 'concat' or 'concat_wo_option':
             for batch in train_loader:
                 optimizer.zero_grad()
@@ -135,6 +171,70 @@ def main(args):
             if args.save_folder:
                 torch.save(model.layer_weights.data, os.path.join(args.save_floder, f"{args.subject}_epoch{epoch+1}.pth"))
             print(f"End of Epoch {epoch+1}, Layer Weights:", model.layer_weights.data)
+
+            for batch in train_loader:
+                optimizer.zero_grad()
+                input_ids = batch["input_ids"].to(device)
+                prefix_ids_len = batch["prefix_ids_len"].to(device)
+                attention_mask = batch["attention_mask"].to(device)
+                label = batch["label"].to(device)
+
+                # batch
+                all_batch = []
+                all_batch_norm = []
+                for i in range(attention_mask.size(0)):
+                    input_ids_sample = input_ids[i]
+                    attention_mask_sample = attention_mask[i]
+                    prefix_len = prefix_ids_len[i]
+                    answer_ids = input_ids_sample[:, prefix_len-1:]
+                    real_answer_ids = []
+                    for row in answer_ids:
+                        idx = (row != 2).nonzero(as_tuple=True)[0].max().item()
+                        row = row[:idx+1]
+                        real_answer_ids.append(row)
+
+                    outputs = model(input_ids=input_ids_sample, attention_mask=attention_mask_sample)
+
+                    # option
+                    one_batch = []
+                    one_batch_norm = []
+                    for j in range(attention_mask_sample.size(0)):
+                        num = len(real_answer_ids[j])
+                        start_idx = prefix_ids_len[i].to(device)
+                        idx_range = torch.arange(num).unsqueeze(0).expand(1, num).to(device)
+                        start_idx_tensor = start_idx.clone().unsqueeze(0).expand(1, num) - 2
+                        final_idx = start_idx_tensor + idx_range
+                        
+                        logits_selected = outputs.logits[j][final_idx]
+                        real_answer_ids[j] = real_answer_ids[j].unsqueeze(0)
+                        logits_selected = logits_selected[0, torch.arange(num), real_answer_ids[j].squeeze(0)]
+                        logits_selected = logits_selected.sum()
+                        logits_selected_norm = logits_selected.sum() / num
+                        
+                        one_batch.append(logits_selected)
+                        one_batch_norm.append(logits_selected_norm)
+                    one_batch = torch.stack(one_batch)
+                    one_batch_norm = torch.stack(one_batch_norm)
+                    all_batch.append(one_batch)
+                    all_batch_norm.append(one_batch_norm)
+                all_batch = torch.stack(all_batch, dim=0)
+                all_batch_norm = torch.stack(all_batch_norm, dim=0)
+                logits = all_batch.to(device)
+                logits_norm = all_batch_norm.to(device)
+
+                batch_accuracy = compute_accuracy(logits, label)
+                total_correct += (batch_accuracy * label.size(0))
+                total_samples += label.size(0)
+
+                batch_accuracy_norm = compute_accuracy(logits_norm, label)
+                total_correct_norm += (batch_accuracy_norm * label.size(0))
+
+        epoch_accuracy = total_correct / total_samples
+        print(f"Epoch{epoch+1} Accuracy: {epoch_accuracy * 100:.2f}%")
+
+        if args.method == 'concat' or 'concat_wo_option':
+            epoch_accuracy_norm = total_correct_norm / total_samples
+        print(f"Epoch{epoch+1} Accuracy_Norm: {epoch_accuracy_norm * 100:.2f}%")
             
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
