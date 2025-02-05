721da00682ff0e36d75f6cbab81dc2157b55cf67
zhujingze <73783778+zhujingze@users.noreply.github.com>
2025-02-05 21:53:12 +0800

    Update ddp.sh

--- ddp.sh
+++ ddp.sh
@@ -1,4 +1,4 @@
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=1  ddp.py \
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8  ddp.py \
     --total_epochs 10 \
     --save_every 1 \
     --batch_size 1 \
