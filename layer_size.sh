970a21883adcdc98acc73489390296b8cf73c5ad
zhujingze <73783778+zhujingze@users.noreply.github.com>
2025-02-09 15:34:34 +0800

    Create layer_size.sh

---
+++ layer_size.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# 循环 num 从 19 到 31
+for num in {19..31}; do
+    # 创建输出目录（如果不存在）
+    mkdir -p "data/math/${num}"
+    
+    # 定义输出文件路径
+    output_file="data/math/${num}/math.txt"
+    
+    # 执行命令并重定向输出
+    CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 torchrun --standalone --nproc_per_node=8 ddp.py \
+        --total_epochs 10 \
+        --save_every 1 \
+        --batch_size 1 \
+        --model 'Llama-2-7b' \
+        --data_folder 'data' \
+        --subject 'math' \
+        --lr 1e-3 \
+        --save_folder 'ckp' \
+        --method "letter" \
+        --num "${num}" > "${output_file}" 2>&1
+    
+    # 可选：添加间隔（例如等待10秒）
+    sleep 10
+done
